d3efb40cfde00a8958949c3185009a2b7c26f639
Omar Besbes <omar.besbes@insat.ucar.tn>
2022-12-05 15:15:15 +0100

    feat: add /Person/all route

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tp3_dotnet.Models;
+
+namespace tp3_dotnet.Controllers;
+
+public class PersonController : Controller
+{
+    private readonly IConfiguration _configuration;
+
+    /* Getting configuration using dependency injection */
+    public PersonController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpGet]
+    [Route("Person/all")]
+    public IActionResult Index()
+    {
+        var allPersons = new Personal_info(_configuration).GetAllPerson();
+        return View(allPersons);
+    }
+}
